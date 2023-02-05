4c1e0ee74d3dcde2f991c182cd759503d958006e
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2023-02-04 22:19:02 -0600

    Fix error messages

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ export const onPostBuild = function({ constants }) {
     console.log("Attempting to append _redirects to dist/_redirects...")
     fs.readFile("/opt/build/repo/_redirects", "utf-8", function (err, data) {
         if (err) {
-            console.log("Could not read _redirects, do you have on in the root of your project?")
+            console.log("Failed to read redirects, do you have a '_redirects' file in the root of your project?")
             throw err
         }
         fs.appendFile(`/opt/build/repo/${constants.PUBLISH_DIR}/_redirects`, "\n" + data, function (err) {
             if (err) {
-                console.log("Could not append _redirects to dist/_redirects")
+                console.log("Fail")
                 throw err
             }
-            console.log("Successfully appended _redirects to dist/_redirects");
+            console.log("Success");
         });
     });
 }
